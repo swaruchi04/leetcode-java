ce41882839926d231e4dd422ca67e41552208615
Swaruchi Ghule <ghuleswaruchi@gmail.com>
2026-02-20 11:02:26 +0530

    Add Java solution for finding first and last occurrence using binary search

---
+++ Arrays/FirstLastPosition.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+class FirstLastPosition {
+
+    public static int[] searchRange(int[] nums, int target) {
+        return new int[]{findFirst(nums, target), findLast(nums, target)};
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            if (nums[mid] == target) ans = mid;
+        }
+        return ans;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (nums[mid] == target) ans = mid;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        System.out.println(Arrays.toString(searchRange(nums, 8))); // [3,4]
+    }
+}

0820f444171c9f7b6e2fd55269a6ca6bb09b220f
Swaruchi Ghule <ghuleswaruchi@gmail.com>
2026-02-22 20:12:02 +0530

    Add sliding window solution for Longest Substring Without Repeating Characters in Java

---
+++ Strings/LongestSubstring.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb")); // 3
+    }
+}
